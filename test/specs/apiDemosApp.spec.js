a1e3b982478d76417e48b6c04a28c5a0c66b3a18
Kamilly <kamillyr@gmail.com>
2022-06-02 14:07:54 -0300

    Fluxo Hide and Show concluido

--- test/specs/apiDemosApp.spec.js
+++ test/specs/apiDemosApp.spec.js
@@ -1,33 +1,33 @@
 describe("Verifica fluxo App/Alert Dialogs/OK CANCEL DIALOG WITH MESSAGE;", () => {
     before(async () => await driver.launchApp());
 
-    it("deve existir botão app", async () => {
+    xit("deve existir botão app", async () => {
         const visibilidade = await $('android=new UiSelector().text("App").className("android.widget.TextView")').isDisplayed();
         
         expect(visibilidade).toBe(true);
     });
 
-    it("deve ir para a tela App ao clicar no botão", async () => {
+    xit("deve ir para a tela App ao clicar no botão", async () => {
         await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
         const visibilidade = await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').isDisplayed();
 
         expect(visibilidade).toBe(true);
     });
 
-    it("deve existir botão Alert Dialogs", async () => {
+    xit("deve existir botão Alert Dialogs", async () => {
         const visibilidade = await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').isDisplayed();
 
         expect(visibilidade).toBe(true);
     });
 
-    it("deve ir para a tela Alert Dialogs ao clicar no botão", async () => {
+    xit("deve ir para a tela Alert Dialogs ao clicar no botão", async () => {
         await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
         const visibilidade = await $('android=new UiSelector().text("App/Alert Dialogs").className("android.widget.TextView")').isDisplayed();
 
         expect(visibilidade).toBe(true);
     });
 
-    it("deve exibir Alert ao clicar no botão CANCEL DIALOG WITH MESSAGE ", async () => {
+    xit("deve exibir Alert ao clicar no botão CANCEL DIALOG WITH MESSAGE ", async () => {
         await $("id=two_buttons").click()
         const visibilidade = await $('android=new UiSelector().textContains("Lorem ipsum dolo").className("android.widget.TextView")').isDisplayed()
     
@@ -37,8 +37,48 @@ describe("Verifica fluxo App/Alert Dialogs/OK CANCEL DIALOG WITH MESSAGE;", () =
     after(async () => await driver.closeApp());
 });
 
-
 describe("Verifica fluxo App/Alert Dialogs/LIST DIALOG", () => {
+    before(async () => await driver.launchApp());
+
+    xit("deve existir botão app", async () => {
+        await driver.launchApp();
+
+        const visibilidade = await $('android=new UiSelector().text("App").className("android.widget.TextView")').isDisplayed();
+        
+        expect(visibilidade).toBe(true);
+    });
+
+    xit("deve ir para a tela App ao clicar no botão", async () => {
+        await $('android=new UiSelector().text("App").className("android.widget.TextView")').click();
+        const visibilidade = await $('android=new UiSelector().text("Action Bar").className("android.widget.TextView")').isDisplayed();
+
+        expect(visibilidade).toBe(true);
+    });
+
+    xit("deve existir botão Alert Dialogs", async () => {
+        const visibilidade = await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').isDisplayed();
+
+        expect(visibilidade).toBe(true);
+    });
+
+    xit("deve ir para a tela Alert Dialogs ao clicar no botão", async () => {
+        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
+        const visibilidade = await $('android=new UiSelector().text("App/Alert Dialogs").className("android.widget.TextView")').isDisplayed();
+
+        expect(visibilidade).toBe(true);
+    });
+
+    xit("deve exibir Alert ao clicar no botão LIST DIALOG ", async () => {
+        await $("id=select_button").click();
+        const visibilidade = await $('android=new UiSelector().text("Header title").className("android.widget.TextView")').isDisplayed();
+    
+        expect(visibilidade).toBe(true);
+    });
+
+    after(async () => await driver.closeApp());
+});
+
+describe("Verifica fluxo  App/Fragment/Hide and Show", () => {
     before(async () => await driver.launchApp());
 
     it("deve existir botão app", async () => {
@@ -56,25 +96,27 @@ describe("Verifica fluxo App/Alert Dialogs/LIST DIALOG", () => {
         expect(visibilidade).toBe(true);
     });
 
-    it("deve existir botão Alert Dialogs", async () => {
-        const visibilidade = await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').isDisplayed();
+    it("deve clicar no botão Fragment", async () => {
+        const visibilidade = await $('android=new UiSelector().text("Fragment").className("android.widget.TextView")').isDisplayed();
+        await $('android=new UiSelector().text("Fragment").className("android.widget.TextView")').click();
+        expect(visibilidade).toBe(true);
+    });
+
+  
+    it("deve clicar em  Hide and Show ", async () => {
+        const visibilidade = await $('android=new UiSelector().text("Hide and Show").className("android.widget.TextView")').isDisplayed();
+        await $('android=new UiSelector().text("Hide and Show").className("android.widget.TextView")').click();
+        
+        expect(visibilidade).toBe(true);
+    });
+
+    it("deve ir para a tela Hide and Show", async () => {
+        await $("id=android:id/action_bar").click();
+        const visibilidade = await $('android=new UiSelector().text("App/Fragment/Hide and Show").className("android.widget.TextView")').isDisplayed();
 
         expect(visibilidade).toBe(true);
     });
 
-    it("deve ir para a tela Alert Dialogs ao clicar no botão", async () => {
-        await $('android=new UiSelector().text("Alert Dialogs").className("android.widget.TextView")').click();
-        const visibilidade = await $('android=new UiSelector().text("App/Alert Dialogs").className("android.widget.TextView")').isDisplayed();
-
-        expect(visibilidade).toBe(true);
-    });
-
-    it("deve exibir Alert ao clicar no botão LIST DIALOG ", async () => {
-        await $("id=select_button").click();
-        const visibilidade = await $('android=new UiSelector().text("Header title").className("android.widget.TextView")').isDisplayed();
-    
-        expect(visibilidade).toBe(true);
-    });
 
     after(async () => await driver.closeApp());
 });
